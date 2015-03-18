834a2a95d2b2f7ef27da0662f47e246c95484b35
Denis Averkov <chstudio@mail.ru>
2015-03-18 16:17:21 +0300

    Фильтр по производителю и по цвету

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,16 @@
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
 
+function unique(arr, key) {
+    var obj = {};
+    for (var i = 0; i < arr.length; i++) {
+        obj[arr[i][key]] = true;
+    }
+    return Object.keys(obj);
+}
+
 phonecatApp.controller('PhoneListCtrl', function($scope) {
     $scope.title = 'Телефоны';
-    $scope.dropdown1_title = 'Производитель';
 	$scope.phones = [
 		{'name': 'Nokia 3310',
             'price': 1500,
@@ -28,4 +35,10 @@ phonecatApp.controller('PhoneListCtrl', function($scope) {
             'color': 'Оранжевый',
             'about': 'Навороченный телефон'}
 	];
+
+    $scope.dropdown1_title = 'Производитель:';
+    $scope.dropdown2_title = 'Цвет:';
+
+    $scope.dropdown1_uniq = unique($scope.phones, 'producer');
+    $scope.dropdown2_uniq = unique($scope.phones, 'color');
 });
