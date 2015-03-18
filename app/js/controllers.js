7015dacdbbc1aad7703e0f163a522b104c9fa8e0
Denis Averkov <chstudio@mail.ru>
2015-03-18 13:30:28 +0300

    Модель с 4 телефонами и unit тестом

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,18 @@
 'use strict';
 
 /* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+    $scope.title = 'Телефоны';
+	$scope.phones = [
+		{'name': 'Nokia 3310',
+		 'snippet': 'Простой и популярный'},
+		{'name': 'Nokia 5530',
+		 'snippet': 'Простой и сенсорный'},
+		{'name': 'Siemens A10',
+		 'snippet': 'Самый простой'},
+        {'name': 'Siemens M55',
+            'snippet': 'Навороченный телефон'}
+	];
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,15 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('PhoneListCtrl', function(){
 
-  it("should do something", function() {
+    beforeEach(module('phonecatApp'));
 
-  });
+    it('Create model "phones" with 4 items', inject(function($controller) {
+        var scope = {},
+            ctrl = $controller('PhoneListCtrl', {$scope:scope});
+
+        expect(scope.phones.length).toBe(4);
+    }));
 
 });
